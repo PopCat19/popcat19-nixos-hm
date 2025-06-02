630f9a169b8ae955b50b3066a3dc236e3f2b07d6
PopCat19 <atsuo11111@gmail.com>
2025-06-02 01:02:53 -0400

    hope mimeApps enabled

--- home.nix
+++ home.nix
@@ -13,6 +13,7 @@
   };
 
   # Declaratively set Zen Browser as the default
+  xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
     "x-scheme-handler/http" = ["app.zen_browser.zen.desktop"];
     "x-scheme-handler/https" = ["app.zen_browser.zen.desktop"];
