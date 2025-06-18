677621d1615e649d245c7849ad91d2c4b41308a8
PopCat19 <atsuo11111@gmail.com>
2025-06-18 13:47:27 -0400

    rm opensssh

--- configuration.nix
+++ configuration.nix
@@ -229,8 +229,8 @@ users.users.popcat19 = {
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.settings.PasswordAuthentication = false;
+  # services.openssh.enable = true;
+  # services.openssh.settings.PasswordAuthentication = false;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
