05608abbe532429ffc87c516d068b979a9a7022d
PopCat19 <atsuo11111@gmail.com>
2025-06-21 15:21:18 -0400

    Restore comprehensive HyprPanel configuration from previous setup
    - Restore layout with dashboard, workspaces, media on left and network, bluetooth, volume, systray, clock, notifications on right
    - Configure Rose Pine theme with 80% scaling and Noto Sans font
    - Set bar auto-hide to fullscreen mode
    - Configure workspace settings with numbered indicators
    - Add dashboard shortcuts for Zen Browser, Kitty, Vesktop, and Fuzzel
    - Configure weather with Suffolk location and API key
    - Set up notifications and power menu settings
    - Configure theme scaling and appearance settings
    - Enable OSD with horizontal orientation

--- home-hyprpanel.nix
+++ home-hyprpanel.nix
@@ -33,33 +33,152 @@
     # See 'https://hyprpanel.com/configuration/settings.html'.
     # Default: <same as gui>
     settings = {
-      bar.launcher.autoDetectIcon = true;
-      bar.workspaces.show_icons = true;
+      # General settings
+      tear = false;
+      scalingPriority = "hyprland";
 
+      # Bar configuration
+      bar.autoHide = "fullscreen";
+      bar.launcher.autoDetectIcon = true;
+      bar.launcher.icon = "";
+
+      # Workspace configuration
+      bar.workspaces.showAllActive = false;
+      bar.workspaces.showWsIcons = false;
+      bar.workspaces.show_icons = false;
+      bar.workspaces.monitorSpecific = false;
+      bar.workspaces.workspaces = 2;
+      bar.workspaces.workspaceMask = false;
+      bar.workspaces.show_numbered = true;
+      bar.workspaces.numbered_active_indicator = "highlight";
+
+      # Media configuration
+      bar.media.format = "{title} {artist: - }";
+      bar.media.truncation = false;
+      bar.media.show_label = true;
+      bar.media.show_active_only = true;
+
+      # Network configuration
+      bar.network.showWifiInfo = true;
+      bar.network.truncation_size = 7;
+      bar.network.truncation = false;
+      bar.network.label = false;
+
+      # Other bar modules
+      bar.bluetooth.label = false;
+      bar.volume.label = true;
+      bar.clock.showIcon = true;
+      bar.clock.showTime = true;
+      bar.clock.format = "%a %m/%d %H:%M";
+      bar.notifications.show_total = true;
+      bar.notifications.hideCountWhenZero = true;
+      bar.customModules.microphone.label = false;
+
+      # Clock and weather
       menus.clock = {
         time = {
           military = true;
           hideSeconds = true;
         };
-        weather.unit = "metric";
+        weather = {
+          enabled = true;
+          location = "Suffolk";
+          key = "dde14cc79e324028be572340252405";
+          unit = "metric";
+        };
       };
 
+      # Menu transitions
+      menus.transition = "crossfade";
+
+      # Media menu
+      menus.media.displayTimeTooltip = true;
+
+      # Volume menu
+      menus.volume.raiseMaximumVolume = true;
+
+      # Power menu
+      menus.power.confirmation = false;
+      menus.power.showLabel = false;
+
+      # Dashboard configuration
       menus.dashboard.directories.enabled = false;
-      menus.dashboard.stats.enable_gpu = true;
+      menus.dashboard.stats.enable_gpu = false;
+      menus.dashboard.powermenu.confirmation = false;
+      menus.dashboard.controls.enabled = true;
 
+      # Dashboard shortcuts
+      menus.dashboard.shortcuts.left.shortcut1.icon = "";
+      menus.dashboard.shortcuts.left.shortcut1.command = "flatpak run app.zen_browser.zen";
+      menus.dashboard.shortcuts.left.shortcut1.tooltip = "Zen Browser";
+      menus.dashboard.shortcuts.left.shortcut2.command = "kitty";
+      menus.dashboard.shortcuts.left.shortcut2.icon = "";
+      menus.dashboard.shortcuts.left.shortcut2.tooltip = "Kitty Terminal";
+      menus.dashboard.shortcuts.left.shortcut3.command = "vesktop";
+      menus.dashboard.shortcuts.left.shortcut3.tooltip = "Vesktop";
+      menus.dashboard.shortcuts.left.shortcut4.command = "fuzzel";
+
+      # Notifications
+      notifications.showActionsOnHover = false;
+      notifications.active_monitor = true;
+      notifications.displayedTotal = 10;
+      notifications.position = "top right";
+
+      # Wallpaper
+      wallpaper.enable = false;
+
+      # Theme configuration
       theme.name = "rose_pine";
-      theme.bar.transparent = true;
       theme.font = {
-        name = "CaskaydiaCove NF";
-        size = "16px";
+        name = "Noto Sans";
+        size = "1.0rem";
       };
 
+      # Bar theme settings
+      theme.bar.floating = false;
+      theme.bar.opacity = 40;
+      theme.bar.transparent = false;
+      theme.bar.outer_spacing = "0.8em";
+      theme.bar.scaling = 80;
+      theme.bar.border_radius = "1.2em";
+
+      # Button theme settings
+      theme.bar.buttons.enableBorders = false;
+      theme.bar.buttons.y_margins = "0.4em";
+      theme.bar.buttons.spacing = "0.4em";
+      theme.bar.buttons.opacity = 100;
+      theme.bar.buttons.workspaces.smartHighlight = true;
+      theme.bar.buttons.modules.netstat.enableBorder = true;
+
+      # Menu theme settings
+      theme.bar.menus.popover.scaling = 64;
+      theme.bar.menus.monochrome = false;
+      theme.bar.menus.menu.dashboard.scaling = 80;
+      theme.bar.menus.menu.media.scaling = 80;
+      theme.bar.menus.menu.clock.scaling = 80;
+      theme.bar.menus.menu.power.scaling = 100;
+      theme.bar.menus.menu.notifications.pager.show = true;
+
+      # Notification theme
+      theme.notification.scaling = 80;
+      theme.notification.border_radius = "1.2em";
+
+      # OSD theme
+      theme.osd.enable = true;
+      theme.osd.location = "top";
+      theme.osd.orientation = "horizontal";
+      theme.osd.muted_zero = false;
+      theme.osd.margins = "32px";
+      theme.osd.active_monitor = true;
+      theme.osd.monitor = 1;
+
+      # Layout configuration
       layout = {
         "bar.layouts" = {
-          "0" = {
-            left = [ "dashboard" "workspaces" ];
-            middle = [ "media" ];
-            right = [ "volume" "systray" "notifications" ];
+          "*" = {
+            left = [ "dashboard" "workspaces" "media" ];
+            middle = [ ];
+            right = [ "network" "bluetooth" "volume" "systray" "clock" "notifications" ];
           };
         };
       };
