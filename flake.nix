b63511d51ba6740a52a665e1a6c12fe0d7bbde03
PopCat19 <atsuo11111@gmail.com>
2025-07-05 19:49:32 -0400

    bump flake

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, hyprpanel, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       # **SYSTEM DEFINITIONS**
       # Basic system parameters for configuration.
