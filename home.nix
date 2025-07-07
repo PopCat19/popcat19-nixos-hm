509a1e07b965cc74f8d9b49d4dab5afac918eac8
PopCat19 <atsuo11111@gmail.com>
2025-07-06 20:32:54 -0400

    aaa

--- home.nix
+++ home.nix
@@ -20,9 +20,9 @@
     WEBKIT_DISABLE_COMPOSITING_MODE = "1";
 
     # Input Method (fcitx5)
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
+    GTK_IM_MODULE = lib.mkForce "fcitx5";
+    QT_IM_MODULE = lib.mkForce "fcitx5";
+    XMODIFIERS = lib.mkForce "@im=fcitx5";
   };
 
   # Add local bin to PATH
