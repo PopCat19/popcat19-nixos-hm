9c6f849597f5e584ad0133bcc0ee3b32b38ea4d1
PopCat19 <atsuo11111@gmail.com>
2025-07-14 03:45:31 -0400

    get cd

--- fish_functions/needy.fish
+++
@@ -1,4 +0,0 @@
-function needy --wraps='yay -Sy --needed' --wraps='yay -Sy --needed --noconfirm' --description 'alias needy yay -Sy --needed --noconfirm'
-  yay -Sy --needed --noconfirm $argv
-        
-end
