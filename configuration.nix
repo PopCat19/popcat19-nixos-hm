a97c2540365baa48cb9494a0b1787c3b2fe93939
PopCat19 <atsuo11111@gmail.com>
2025-07-27 02:09:49 -0400

    aaaaA

--- configuration.nix
+++ configuration.nix
@@ -363,6 +363,7 @@
   nix.settings = {
     substituters = [ "https://ezkea.cachix.org" ];
     trusted-public-keys = [ "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI=" ];
+    download-buffer-size = 67108864;
   };
 
   environment.sessionVariables = {
