8c2087c3c0b3b5ac5950ed9647a547999802e5ce
PopCat19 <atsuo11111@gmail.com>
2025-07-29 04:40:12 -0400

    more modules1

---
+++ modules/fish.nix
@@ -0,0 +1,83 @@
+{ pkgs, ... }:
+
+{
+  # Fish Shell Configuration
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+      set -Ux NIXOS_CONFIG_DIR $HOME/nixos-config
+      set -Ux NIXOS_FLAKE_HOSTNAME popcat19-nixos0
+      set -g fish_greeting "" # Disable default fish greeting.
+
+      # Custom greeting disabled - fastfetch removed
+      function fish_greeting
+          # Empty greeting
+      end
+      fish_add_path $HOME/bin # Add user's bin directory to PATH.
+      fish_add_path $HOME/.npm-global/bin # Add npm global packages to PATH.
+      if status is-interactive
+          starship init fish | source # Initialize Starship prompt.
+      end
+    '';
+    # Custom shell abbreviations for convenience.
+    shellAbbrs = {
+      # Navigation shortcuts.
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      ".3" = "cd ../../..";
+      ".4" = "cd ../../../..";
+      ".5" = "cd ../../../../..";
+
+      # File Operations using eza.
+      mkdir = "mkdir -p";
+      l = "eza -lh --icons=auto";
+      ls = "eza -1 --icons=auto";
+      ll = "eza -lha --icons=auto --sort=name --group-directories-first";
+      ld = "eza -lhD --icons=auto";
+      lt = "eza --tree --icons=auto";
+      o = "open_smart"; # Custom function to open files.
+
+      # NixOS Configuration Management.
+      nconf = "nixconf-edit";
+      nixos-ed = "nixconf-edit";
+      hconf = "homeconf-edit";
+      home-ed = "homeconf-edit";
+      flconf = "flake-edit";
+      flake-ed = "flake-edit";
+      flup = "flake-update";
+      flake-up = "flake-update";
+      ngit = "nixos-git";
+
+      # NixOS Build and Switch operations.
+      nrb = "nixos-apply-config";
+      nixos-sw = "nixos-apply-config";
+      nerb = "nixos-edit-rebuild";
+      nixoss = "nixos-edit-rebuild";
+      herb = "home-edit-rebuild";
+      home-sw = "home-edit-rebuild";
+      nup = "nixos-upgrade";
+      nixos-up = "nixos-upgrade";
+
+      # Package Management with nixpkg.
+      pkgls = "nixpkg list";
+      pkgadd = "nixpkg add";
+      pkgrm = "nixpkg remove";
+      pkgs = "nixpkg search";
+      pkghelp = "nixpkg help";
+      pkgman = "nixpkg manual";
+      pkgaddr = "nixpkg add";
+      pkgrmr = "nixpkg remove";
+    };
+  };
+
+  # Fish configuration files
+  home.file.".config/fish/functions" = {
+    source = ./fish_functions;
+    recursive = true;
+  };
+
+  home.file.".config/fish/themes" = {
+    source = ./fish_themes;
+    recursive = true;
+  };
+}
---
+++ modules/micro.nix
@@ -0,0 +1,32 @@
+{ pkgs, config, ... }:
+
+{
+  # Micro text editor configuration
+  programs.micro = {
+    enable = true;
+    settings = {
+      colorscheme = "rose-pine";
+      mkparents = true;
+      softwrap = true;
+      wordwrap = true;
+      tabsize = 4;
+      autoclose = true;
+      autoindent = true;
+      autosave = 5;
+      clipboard = "terminal";
+      cursorline = true;
+      diffgutter = true;
+      ignorecase = true;
+      scrollbar = true;
+      smartpaste = true;
+      statusline = true;
+      syntax = true;
+      tabstospaces = true;
+    };
+  };
+
+  # Micro editor colorscheme
+  home.file.".config/micro/colorschemes/rose-pine.micro" = {
+    source = ./micro_config/rose-pine.micro;
+  };
+}
