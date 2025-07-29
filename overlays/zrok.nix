e3d1bdd15d353747db66afe3ab967d722670c22d
PopCat19 <atsuo11111@gmail.com>
2025-07-29 08:06:58 -0400

    hash fix1

--- overlays/zrok.nix
+++ overlays/zrok.nix
@@ -2,33 +2,39 @@
 # https://github.com/openziti/zrok
 
 final: prev: {
-  zrok = prev.buildGoModule rec {
+  zrok = prev.stdenv.mkDerivation rec {
     pname = "zrok";
     version = "1.0.7";
 
-    src = prev.fetchFromGitHub {
-      owner = "openziti";
-      repo = "zrok";
-      rev = "v${version}";
-      hash = "sha256-IY9eZyEm8cAixt+Vwr50UVjrA31cHfm715zpqdMieUs=";
+    src = prev.fetchurl {
+      url = "https://github.com/openziti/zrok/releases/download/v${version}/zrok_${version}_linux_amd64.tar.gz";
+      hash = "sha256-tQMPvUSd2J8iCX8QfHEv/k8DjXk3kUNzGMSRr2Gx2qU=";
     };
 
-    vendorHash = "sha256-bU6WXe5hhnGX7hcbtDlnl1vuxjJOMhaWAts8+fUjpdk=";
+    dontUnpack = true;
+    nativeBuildInputs = [ prev.patchelf ];
 
-    subPackages = [ "cmd/zrok" ];
+    installPhase = ''
+      runHook preInstall
 
-    ldflags = [
-      "-s"
-      "-w"
-      "-X github.com/openziti/zrok/build.Version=${version}"
-    ];
+      tar -xzf $src -C .
+      mkdir -p $out/bin
+      cp ./zrok $out/bin/zrok
+      chmod +x $out/bin/zrok
+
+      # Set correct ELF interpreter for NixOS
+      patchelf --set-interpreter "$(< $NIX_CC/nix-support/dynamic-linker)" "$out/bin/zrok"
+
+      runHook postInstall
+    '';
 
     meta = with prev.lib; {
       description = "Geo-scale, next-generation sharing platform built on top of OpenZiti";
       homepage = "https://zrok.io";
       license = licenses.asl20;
       maintainers = with maintainers; [ ];
-      platforms = platforms.unix;
+      platforms = [ "x86_64-linux" ];
+      sourceProvenance = with prev.lib.sourceTypes; [ binaryNativeCode ];
       mainProgram = "zrok";
     };
   };
