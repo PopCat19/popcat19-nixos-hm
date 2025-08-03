7f78a2c238bae5c00ed92bec4ff165dbebddbc2e
PopCat19 <atsuo11111@gmail.com>
2025-08-03 01:58:24 -0400

    Fix SSH connection from nixos0 to surface0

--- home_modules/ssh.nix
+++ home_modules/ssh.nix
@@ -21,6 +21,19 @@
         ControlMaster auto
         ControlPath ~/.ssh/master-%r@%h:%p
         ControlPersist 600
+
+      # Host configuration for surface0
+      Host 192.168.50.219
+        User popcat19
+        IdentityFile ~/.ssh/id_ed25519
+        StrictHostKeyChecking no
+        UserKnownHostsFile /dev/null
+        ForwardX11 no
+        ForwardX11Trusted no
+        Compression yes
+        ControlMaster auto
+        ControlPath ~/.ssh/master-%r@%h:%p
+        ControlPersist 600
     '';
   };
 }
