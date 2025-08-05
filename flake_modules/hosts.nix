f3ecf2cab39d4495b33f62eb01cfd771e4ddeb73
PopCat19 <atsuo11111@gmail.com>
2025-08-05 00:43:27 -0400

    modular flake

---
+++ flake_modules/hosts.nix
@@ -0,0 +1,44 @@
+{
+  # System configuration generator
+  mkSystemConfig = system: userConfig: { inputs, nixpkgs, modules }:
+  nixpkgs.lib.nixosSystem {
+    inherit system;
+    specialArgs = { inherit inputs userConfig; };
+
+    modules = [
+      { nixpkgs.overlays = import ./overlays.nix system; }
+
+      # External modules
+      inputs.chaotic.nixosModules.default
+      inputs.home-manager.nixosModules.home-manager
+
+      # Feature modules
+      (modules.mkGamingModule system { inherit inputs; })
+      (modules.mkHomeManagerModule system { 
+        inherit userConfig;
+        username = userConfig.user.username;
+      })
+    ];
+  };
+
+  # Host-specific configuration generator
+  mkHostConfig = hostname: system: hostConfigPath: { inputs, nixpkgs, modules }:
+  nixpkgs.lib.nixosSystem {
+    inherit system;
+    specialArgs = { inherit inputs; };
+
+    modules = [
+      { nixpkgs.overlays = import ./overlays.nix system; }
+
+      # Host-specific configuration
+      hostConfigPath
+
+      # External modules
+      inputs.chaotic.nixosModules.default
+      inputs.home-manager.nixosModules.home-manager
+
+      # Feature modules
+      (modules.mkGamingModule system { inherit inputs; })
+    ];
+  };
+}
