037e639ebc13cab8bcc72f12192d0d07274155e0
PopCat19 <atsuo11111@gmail.com>
2025-08-13 00:46:51 -0400

    move ollama to generative module

---
+++ home_modules/generative.nix
@@ -0,0 +1,21 @@
+{ system, pkgs, ... }:
+
+let
+  isX86_64 = system == "x86_64-linux";
+  # ROCm on x86_64 only
+  ollamaAcceleration = if isX86_64 then "rocm" else null;
+in
+{
+  # Generative/AI related packages and services
+  home.packages = with pkgs; [
+    ollama-rocm
+  ];
+
+  services = {
+    # AI/ML Services
+    ollama = {
+      enable = true;
+      acceleration = ollamaAcceleration; # ROCm on x86_64, CPU on other architectures
+    };
+  };
+}
