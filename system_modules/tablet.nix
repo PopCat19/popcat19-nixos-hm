688cea2aaf3bfc41d4b1f1e552ad2c427268981b
PopCat19 <atsuo11111@gmail.com>
2025-08-13 03:45:48 -0400

    attempt otd without udev rules

--- system_modules/tablet.nix
+++ system_modules/tablet.nix
@@ -4,8 +4,8 @@
   # Tablet configuration
   hardware.opentabletdriver.enable = true;
 
-  # Udev rules for tablet
-  services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a" , ATTRS{idProduct}=="efe8", MODE="0666", GROUP="users"
-  '';
+  # # Udev rules for tablet
+  # services.udev.extraRules = ''
+  #   SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a" , ATTRS{idProduct}=="efe8", MODE="0666", GROUP="users"
+  # '';
 }
