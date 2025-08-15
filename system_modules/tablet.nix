58f0fd5b91b24901be2236430a9b59d1624524e1
PopCat19 <atsuo11111@gmail.com>
2025-08-15 04:21:43 -0400

    revert nixpkgs_3 to fix OTD issues

--- system_modules/tablet.nix
+++ system_modules/tablet.nix
@@ -7,10 +7,4 @@
   daemon.enable = true;
   package = pkgs.opentabletdriver;
   };
-
-  # Udev rules for tablet
-  services.udev.extraRules = ''
-    # Gaomon S620
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="006d", MODE="0666", GROUP="users"
-  '';
 }
