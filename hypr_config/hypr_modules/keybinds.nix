202824a45e68fad24582beeca4e2e7f3ed78ad83
PopCat19 <atsuo11111@gmail.com>
2025-08-22 17:01:34 -0400

    attempt fixes

--- hypr_config/hypr_modules/keybinds.nix
+++ hypr_config/hypr_modules/keybinds.nix
@@ -1,6 +1,5 @@
 # Key Bindings for Hyprland
 # ==========================
-
 {
   wayland.windowManager.hyprland.settings = {
     # Main modifier key
@@ -11,7 +10,7 @@
     "$editor" = "micro";
     "$file" = "dolphin";
     "$browser" = "zen-twilight";
-    "$menu" = "fuzzel";
+    "$menu" = "fuzzel --dmenu";
     "$launcher" = "vicinae";
 
     # Key bindings
