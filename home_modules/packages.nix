768d21296c3baac648b50135c1470866d123ae09
PopCat19 <atsuo11111@gmail.com>
2025-08-29 04:13:17 -0400

    lazyvim

--- home_modules/packages.nix
+++ home_modules/packages.nix
@@ -77,4 +77,22 @@ with pkgs;
   # Nix Development
   nil
   nixd
+   nixfmt-rfc-style
+
+   # Neovim and Language Servers
+   neovim
+   rust-analyzer
+   pyright
+   lua-language-server
+   nodePackages_latest.typescript-language-server
+   nodePackages_latest.vscode-langservers-extracted
+
+   # Code Formatters
+   black
+   prettierd
+
+   # Development Tools
+   ripgrep
+   fd
+   lazygit
 ]
