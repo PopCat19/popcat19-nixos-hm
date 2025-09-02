8ac0cf44ff6decec1a116ad1da481082787be97f
PopCat19 <atsuo11111@gmail.com>
2025-09-01 20:57:17 -0400

    fix: hypr env list remove empty entry

--- hypr_config/hypr_modules/environment.nix
+++ hypr_config/hypr_modules/environment.nix
@@ -2,27 +2,26 @@
 # ==================================
 
 {
-  wayland.windowManager.hyprland.settings = {
-    env = [
-      # Cursor theme
-      "HYPRCURSOR_THEME,rose-pine-hyprcursor"
-      "XCURSOR_SIZE,24"
-      "HYPRCURSOR_SIZE,28"
+wayland.windowManager.hyprland.settings = {
+  env = [
+    # Cursor theme
+    "HYPRCURSOR_THEME,rose-pine-hyprcursor"
+    "XCURSOR_SIZE,24"
+    "HYPRCURSOR_SIZE,28"
 
-      # Desktop session
-      "XDG_CURRENT_DESKTOP,Hyprland"
-      "XDG_SESSION_TYPE,wayland"
-      "XDG_SESSION_DESKTOP,Hyprland"
+    # Desktop session
+    "XDG_CURRENT_DESKTOP,Hyprland"
+    "XDG_SESSION_TYPE,wayland"
+    "XDG_SESSION_DESKTOP,Hyprland"
 
-      # Qt configuration
-      "QT_QPA_PLATFORM,wayland;xcb"
-      ""
-      "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-      "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+    # Qt configuration
+    "QT_QPA_PLATFORM,wayland;xcb"
+    "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+    "QT_AUTO_SCREEN_SCALE_FACTOR,1"
 
-      # Application compatibility
-      "MOZ_ENABLE_WAYLAND,1"
-      "GDK_SCALE,1"
-    ];
-  };
+    # Application compatibility
+    "MOZ_ENABLE_WAYLAND,1"
+    "GDK_SCALE,1"
+  ];
+};
 }
