be23e94de2360727bd542422af95ecc4d91f91a4
PopCat19 <atsuo11111@gmail.com>
2025-09-02 19:11:15 -0400

    feat: preconfigure flatpak with flathub repo

--- system_modules/services.nix
+++ system_modules/services.nix
@@ -41,4 +41,13 @@
   # Security & authentication
   security.polkit.enable = true;
   security.rtkit.enable = true;
+
+  # Add Flathub repository automatically
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 }
