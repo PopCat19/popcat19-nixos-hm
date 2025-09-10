6e6b239436fa40fefb91662d322842c586495bb7
PopCat19 <atsuo11111@gmail.com>
2025-09-10 04:29:33 -0400

    touchup packages.nix

--- system_modules/packages.nix
+++ system_modules/packages.nix
@@ -3,27 +3,22 @@
 { pkgs, userConfig, ... }:
 
 let
-  # Import architecture-specific modules
+  # Architecture-specific packages
   x86_64Packages = import ./x86_64-packages.nix { inherit pkgs; };
 
-  # Architecture detection
-  system = userConfig.host.system;
-  isX86_64 = system == "x86_64-linux";
+  # Maintain ordering while reducing boilerplate
+  packageFiles = [
+    ../packages/system/network.nix
+    ../packages/system/hardware.nix
+    ../packages/system/gui.nix
+    ../packages/system/development.nix
+  ];
 
-  # Virtualization packages moved to system_modules/virtualisation.nix
-  # See system_modules/virtualisation.nix for virtualization-related packages
-
-  # Import categorized package modules
-  networkPackages = import ../packages/system/network.nix { inherit pkgs; };
-  hardwarePackages = import ../packages/system/hardware.nix { inherit pkgs; };
-  guiPackages = import ../packages/system/gui.nix { inherit pkgs; };
-  developmentPackages = import ../packages/system/development.nix { inherit pkgs; };
+  importPackages = path: import path { inherit pkgs; };
+  categorized = map importPackages packageFiles;
 in
 {
-  # All packages are now organized in categorized modules
-  environment.systemPackages = networkPackages
-  ++ hardwarePackages
-  ++ guiPackages
-  ++ developmentPackages
-  ++ x86_64Packages;
+  environment.systemPackages =
+    builtins.concatLists categorized
+    ++ x86_64Packages;
 }
