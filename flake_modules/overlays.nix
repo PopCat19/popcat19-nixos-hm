7068eb31efbab63683876ade1f3a34944013fd0c
PopCat19 <atsuo11111@gmail.com>
2025-09-21 07:50:04 -0400

    feat: configure gtk to rose-pine-gtk-theme-full

--- flake_modules/overlays.nix
+++ flake_modules/overlays.nix
@@ -5,4 +5,6 @@ system: [
   # Import overlays
   # Rosé Pine Kvantum themes (from nixpkgs-unstable recipe)
   (import ../overlays/rose-pine-kvantum.nix)
+  # Rosé Pine full GTK theme (Main & Moon variants with icons)
+  (import ../overlays/rose-pine-gtk-theme-full.nix)
 ]
