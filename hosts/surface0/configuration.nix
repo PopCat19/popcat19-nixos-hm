60e407f1df32ccdb96683ccdbb537b9ab5e1a030
PopCat19 <atsuo11111@gmail.com>
2025-09-21 04:46:28 -0400

    isolate core modules

--- hosts/surface0/configuration.nix
+++ hosts/surface0/configuration.nix
@@ -13,12 +13,12 @@ in {
     ../../syncthing_config/system.nix
     ./system_modules/boot.nix
     ./system_modules/hardware.nix
-    ../../system_modules/networking.nix
+    ../../system_modules/core_modules/networking.nix
     ../../system_modules/localization.nix
     ../../system_modules/services.nix
     ../../system_modules/display.nix
     ../../system_modules/audio.nix
-    ../../system_modules/users.nix
+    ../../system_modules/core_modules/users.nix
     ../../system_modules/programs.nix
     ../../system_modules/environment.nix
     ../../system_modules/core-packages.nix
--- hosts/thinkpad0/configuration.nix
+++ hosts/thinkpad0/configuration.nix
@@ -9,14 +9,14 @@ in {
   imports = [
     ./hardware-configuration.nix
     ../../syncthing_config/system.nix
-    ../../system_modules/boot.nix
-    ../../system_modules/hardware.nix
-    ../../system_modules/networking.nix
+    ../../system_modules/core_modules/boot.nix
+    ../../system_modules/core_modules/hardware.nix
+    ../../system_modules/core_modules/networking.nix
     ../../system_modules/localization.nix
     ../../system_modules/services.nix
     ../../system_modules/display.nix
     ../../system_modules/audio.nix
-    ../../system_modules/users.nix
+    ../../system_modules/core_modules/users.nix
     ../../system_modules/virtualisation.nix
     ../../system_modules/programs.nix
     ../../system_modules/environment.nix
