1f25c30ac604cb56066edaddedd03316d5cf4226
PopCat19 <atsuo11111@gmail.com>
2025-09-21 07:03:59 -0400

    fix: ensure Papirus icon theme for Qt5 and Qt6 via qt5ct/qt6ct

--- home_modules/theme.nix
+++ home_modules/theme.nix
@@ -130,6 +130,35 @@ in {
     };
   };
 
+  # Ensure Qt5 apps also use Kvantum style and Papirus icons
+  home.file.".config/qt5ct/qt5ct.conf" = {
+    text = ''
+      [Appearance]
+      color_scheme_path=
+      custom_palette=false
+      icon_theme=${selectedVariant.iconTheme}
+      style=kvantum
+
+      [Fonts]
+      fixed="${fonts.mono},11,-1,5,50,0,0,0,0,0"
+      general="${fonts.main},${builtins.toString fonts.sizes.gtk},-1,5,50,0,0,0,0,0"
+
+      [Interface]
+      activate_item_on_single_click=1
+      buttonbox_layout=0
+      cursor_flash_time=1000
+      dialog_buttons_have_icons=1
+      double_click_interval=400
+      gui_effects=@Invalid()
+      keyboard_scheme=2
+      menus_have_icons=true
+      show_shortcuts_in_context_menus=true
+      stylesheets=@Invalid()
+      toolbutton_style=4
+      underline_shortcut=1
+      wheel_scroll_lines=3
+    '';
+  };
 
   programs.kitty.font = {
     name = fonts.mono;
