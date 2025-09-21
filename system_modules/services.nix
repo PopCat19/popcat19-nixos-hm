6ddd10a407a0f32a47048ffab7bcdc39b6b63925
PopCat19 <atsuo11111@gmail.com>
2025-09-21 03:18:12 -0400

    attempt flathub service fix

--- system_modules/services.nix
+++ system_modules/services.nix
@@ -43,8 +43,8 @@
   # Add Flathub repository automatically
   systemd.services.flatpak-repo = {
     wantedBy = ["multi-user.target"];
-    after = [ "network-online.target" "mullvad-autoconnect.service" ];
-    wants = [ "network-online.target" "mullvad-autoconnect.service" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
     path = [pkgs.flatpak];
     script = ''
       flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
