1334b392ec07d784d21374e02f6b29e4abe23f44
PopCat19 <atsuo11111@gmail.com>
2025-10-05 20:01:59 -0400

    attempt hdmi audio

--- hosts/nixos0/configuration.nix
+++ hosts/nixos0/configuration.nix
@@ -13,9 +13,20 @@ in {
 
   networking.hostName = "popcat19-nixos0";
 
+  # Boot configuration for HDMI audio support
+  boot = {
+    kernelModules = ["snd_hda_intel" "snd_hda_codec_hdmi"];
+    kernelParams = [
+      "snd_hda_intel.enable=1"
+      "snd_hda_intel.model=auto"
+    ];
+  };
+
   # Host-specific packages
   environment.systemPackages = with pkgs; [
     zluda
+    alsa-utils
+    pavucontrol
   ];
 
   system.stateVersion = "25.05";
--- system_modules/audio.nix
+++ system_modules/audio.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services.pipewire = {
     enable = true;
     alsa = {
@@ -6,5 +6,18 @@
       support32Bit = true;
     };
     pulse.enable = true;
+    # Extra configuration for HDMI audio support
+    extraConfig.pipewire."91-hdmi-audio" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [ 44100 48000 96000 ];
+      };
+    };
   };
+
+  # Ensure ALSA utilities are available for audio management
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+    pavucontrol
+  ];
 }
