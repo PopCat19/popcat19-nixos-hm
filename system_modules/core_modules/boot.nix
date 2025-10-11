bcfeb15394b01d99e4b8b7bd0a1f37097d9c3ace
PopCat19 <atsuo11111@gmail.com>
2025-10-11 04:10:18 -0400

    limit 5 generations

--- system_modules/core_modules/boot.nix
+++ system_modules/core_modules/boot.nix
@@ -4,6 +4,7 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       timeout = 3;
+      configurationLimit = 5;
     };
     supportedFilesystems = ["ntfs"];
     kernelPackages = pkgs.linuxPackages_zen;
