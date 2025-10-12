f5b564dd49d0e1e37c78910dd818e978a73fd76d
PopCat19 <atsuo11111@gmail.com>
2025-10-12 07:45:04 -0400

    cleanup aggregators

--- hosts/nixos0/home.nix
+++ hosts/nixos0/home.nix
@@ -13,11 +13,31 @@
   home.homeDirectory = userConfig.directories.home;
   home.stateVersion = "24.05";
 
-  # Imports (aggregated shared modules + host-local hypr configs)
+  # Imports (all modules for nixos0, including generative AI)
   imports = [
     ./hypr_config/hyprland.nix
     ./hypr_config/hyprpanel.nix
-  ] ++ (import ../../home_modules/default.nix { });
+    ../../home_modules/theme.nix
+    ../../home_modules/fonts.nix
+    ../../home_modules/screenshot.nix
+    ../../home_modules/zen-browser.nix
+    ../../home_modules/generative.nix
+    ../../home_modules/environment.nix
+    ../../home_modules/services.nix
+    ../../home_modules/home-files.nix
+    ../../home_modules/systemd-services.nix
+    ../../home_modules/kde-apps.nix
+    ../../home_modules/qt-gtk-config.nix
+    ../../home_modules/fuzzel-config.nix
+    ../../home_modules/kitty.nix
+    ../../home_modules/fish.nix
+    ../../home_modules/starship.nix
+    ../../home_modules/micro.nix
+    ../../home_modules/fcitx5.nix
+    ../../home_modules/privacy.nix
+    ../../quickshell_config/quickshell.nix
+    ../../syncthing_config/home.nix
+  ];
 
   # Use the centralized packages list from home_modules/packages.nix.
   # This is the canonical source; per-host package overrides can still be
