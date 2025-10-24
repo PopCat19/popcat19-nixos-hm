6e9f59b9336dd7a8142b9dce6a99a2d35ce28081
PopCat19 <atsuo11111@gmail.com>
2025-10-24 01:47:23 -0400

    flake bump

--- overlays/fcitx5-fix.nix
+++ overlays/fcitx5-fix.nix
@@ -1,13 +1,9 @@
-# Fix fcitx5-qt6 build issues by disabling the problematic qt6 module
+# Fix fcitx5-qt6 build issues by overriding the package to exclude qt6
 final: prev: {
-  fcitx5 = prev.fcitx5.override {
-    # Disable qt6 support to avoid build failures
-    withQt6 = false;
-  };
-  
-  # Override fcitx5-with-addons to exclude qt6
-  fcitx5-with-addons = prev.fcitx5-with-addons.override {
-    # Use only Qt5 version
-    qt6Support = false;
-  };
+  # Override fcitx5-with-addons to use only Qt5 version and exclude qt6
+  fcitx5-with-addons = prev.fcitx5-with-addons.overrideAttrs (oldAttrs: {
+    buildInputs = builtins.filter (input: !(builtins.match ".*fcitx5-qt6.*" (input.name or ""))) oldAttrs.buildInputs or [];
+    propagatedBuildInputs = builtins.filter (input: !(builtins.match ".*fcitx5-qt6.*" (input.name or ""))) oldAttrs.propagatedBuildInputs or [];
+    propagatedUserEnvPkgs = builtins.filter (input: !(builtins.match ".*fcitx5-qt6.*" (input.name or ""))) oldAttrs.propagatedUserEnvPkgs or [];
+  });
 }
