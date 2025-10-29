a6d125e8c22ef7e4c2fe5ec3ff469e2af3afb3d0
PopCat19 <atsuo11111@gmail.com>
2025-10-29 00:28:47 -0400

    feat(fcitx5): reenable fcitx5 input method configuration
    - bump flake

--- home_modules/fcitx5.nix
+++ home_modules/fcitx5.nix
@@ -1,58 +1,56 @@
 {pkgs, ...}: {
   # **INPUT METHOD CONFIGURATION**
-  # Temporarily disabled due to fcitx5-qt6 build issues
-  # TODO: Re-enable when fcitx5-qt6 is fixed in nixpkgs
-  # i18n.inputMethod = {
-  #   type = "fcitx5";
-  #   enable = true;
-  #   fcitx5.addons = with pkgs; [
-  #     libsForQt5.fcitx5-qt # Qt module.
-  #     fcitx5-gtk
-  #     fcitx5-mozc # Mozc input method engine for Japanese.
-  #     fcitx5-rose-pine # Rose Pine theme for fcitx5.
-  #   ];
-  # };
+  i18n.inputMethod = {
+    type = "fcitx5";
+    enable = true;
+    fcitx5.addons = with pkgs; [
+      libsForQt5.fcitx5-qt # Qt module.
+      fcitx5-gtk
+      fcitx5-mozc # Mozc input method engine for Japanese.
+      fcitx5-rose-pine # Rose Pine theme for fcitx5.
+    ];
+  };
 
-  # fcitx5 Rose Pine theme configuration (temporarily disabled)
-  # home.file.".config/fcitx5/conf/classicui.conf".text = ''
-  #   # Vertical Candidate List
-  #   Vertical Candidate List=False
-  #   # Use Per Screen DPI
-  #   PerScreenDPI=True
-  #   # Use mouse wheel to go to prev or next page
-  #   WheelForPaging=True
-  #   # Font
-  #   Font="Rounded Mplus 1c Medium 11"
-  #   # Menu Font
-  #   MenuFont="Rounded Mplus 1c Medium 11"
-  #   # Tray Font
-  #   TrayFont="Rounded Mplus 1c Medium 11"
-  #   # Tray Label Outline Color
-  #   TrayOutlineColor=#000000
-  #   # Tray Label Text Color
-  #   TrayTextColor=#ffffff
-  #   # Prefer Text Icon
-  #   PreferTextIcon=False
-  #   # Show Layout Name In Icon
-  #   ShowLayoutNameInIcon=True
-  #   # Use input method language to display text
-  #   UseInputMethodLangaugeToDisplayText=True
-  #   # Rose Pine Theme
-  #   Theme=rose-pine
-  #   # Dark Theme
-  #   DarkTheme=rose-pine
-  #   # Follow system light/dark color scheme
-  #   UseDarkTheme=True
-  #   # Use accent color
-  #   UseAccentColor=True
-  #   # Use system tray icon
-  #   EnableTray=True
-  #   # Show preedit in application
-  #   ShowPreeditInApplication=False
-  # '';
+  # fcitx5 Rose Pine theme configuration
+  home.file.".config/fcitx5/conf/classicui.conf".text = ''
+    # Vertical Candidate List
+    Vertical Candidate List=False
+    # Use Per Screen DPI
+    PerScreenDPI=True
+    # Use mouse wheel to go to prev or next page
+    WheelForPaging=True
+    # Font
+    Font="Rounded Mplus 1c Medium 11"
+    # Menu Font
+    MenuFont="Rounded Mplus 1c Medium 11"
+    # Tray Font
+    TrayFont="Rounded Mplus 1c Medium 11"
+    # Tray Label Outline Color
+    TrayOutlineColor=#000000
+    # Tray Label Text Color
+    TrayTextColor=#ffffff
+    # Prefer Text Icon
+    PreferTextIcon=False
+    # Show Layout Name In Icon
+    ShowLayoutNameInIcon=True
+    # Use input method language to display text
+    UseInputMethodLangaugeToDisplayText=True
+    # Rose Pine Theme
+    Theme=rose-pine
+    # Dark Theme
+    DarkTheme=rose-pine
+    # Follow system light/dark color scheme
+    UseDarkTheme=True
+    # Use accent color
+    UseAccentColor=True
+    # Use system tray icon
+    EnableTray=True
+    # Show preedit in application
+    ShowPreeditInApplication=False
+  '';
 
-  # Manually link fcitx5 rose pine themes to user directory (temporarily disabled)
-  # home.file.".local/share/fcitx5/themes/rose-pine".source = "${pkgs.fcitx5-rose-pine}/share/fcitx5/themes/rose-pine";
-  # home.file.".local/share/fcitx5/themes/rose-pine-dawn".source = "${pkgs.fcitx5-rose-pine}/share/fcitx5/themes/rose-pine-dawn";
-  # home.file.".local/share/fcitx5/themes/rose-pine-moon".source = "${pkgs.fcitx5-rose-pine}/share/fcitx5/themes/rose-pine-moon";
+  # Manually link fcitx5 rose pine themes to user directory
+  home.file.".local/share/fcitx5/themes/rose-pine".source = "${pkgs.fcitx5-rose-pine}/share/fcitx5/themes/rose-pine";
+  home.file.".local/share/fcitx5/themes/rose-pine-dawn".source = "${pkgs.fcitx5-rose-pine}/share/fcitx5/themes/rose-pine-dawn";
+  home.file.".local/share/fcitx5/themes/rose-pine-moon".source = "${pkgs.fcitx5-rose-pine}/share/fcitx5/themes/rose-pine-moon";
 }
