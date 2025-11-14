15a3c51f4e3a9d586260e62acd40ba2e92a4d8fc
PopCat19 <atsuo11111@gmail.com>
2025-11-13 21:10:40 -0500

    feat(nix): add chaotic-nyx cache to substituters
    - Add chaotic-nyx.cachix.org to substituters list
    - Add corresponding public key for Chaotic Nyx cache
    - Enables faster builds and access to CachyOS kernel packages
    - Configuration verified with flake check successful

--- system_modules/environment.nix
+++ system_modules/environment.nix
@@ -20,11 +20,13 @@
       "https://cache.nixos.org"
       "https://shimboot-systemd-nixos.cachix.org"
       "https://ezkea.cachix.org"
+      "https://chaotic-nyx.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "shimboot-systemd-nixos.cachix.org-1:vCWmEtJq7hA2UOLN0s3njnGs9/EuX06kD7qOJMo2kAA="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
     ];
     download-buffer-size = 67108864;
     trusted-users = ["root" userConfig.user.username];
