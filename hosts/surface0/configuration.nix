0629657d5422d5df1570b81368311fc2d4be5b10
PopCat19 <atsuo11111@gmail.com>
2025-12-10 22:24:18 -0500

    refactor(system_modules): clean up and reorganize module structure

--- hosts/surface0/configuration.nix
+++ hosts/surface0/configuration.nix
@@ -17,17 +17,4 @@
   ];
 
   networking.hostName = "popcat19-surface0";
-
-  # Add hyprshade to system packages for surface0 (package provided via overlays/hyprshade.nix)
-  environment.systemPackages = with pkgs; (config.environment.systemPackages or []) ++ [hyprshade];
-
-  # Add nixos0's SSH public key to surface0's authorized_keys
-  users.users.${userConfig.user.username} = {
-    openssh.authorizedKeys.keys = [
-      # Default SSH key from system_modules/ssh.nix
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFvtrt7vEbXSyP8xuOfsfNGgC99Y98s1fmBIp3eZP4zx popcat19@nixos"
-      # nixos0's SSH public key for SSH access from nixos0 to surface0
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFvtrt7vEbXSyP8xuOfsfNGgC99Y98s1fmBIp3eZP4zx popcat19@nixos0"
-    ];
-  };
 }
--- hosts/thinkpad0/configuration.nix
+++ hosts/thinkpad0/configuration.nix
@@ -16,21 +16,6 @@
 
   networking.hostName = "popcat19-thinkpad0";
 
-  # Boot configuration for HDMI audio support
-  boot = {
-    kernelModules = ["snd_hda_intel" "snd_hda_codec_hdmi"];
-    kernelParams = [
-      "snd_hda_intel.enable=1"
-      "snd_hda_intel.model=auto"
-    ];
-  };
-
-  # Host-specific packages for audio management
-  environment.systemPackages = with pkgs; [
-    alsa-utils
-    pavucontrol
-  ];
-
   # Disable Waydroid for thinkpad0 (override from virtualisation module)
   virtualisation.waydroid.enable = lib.mkForce false;
 
