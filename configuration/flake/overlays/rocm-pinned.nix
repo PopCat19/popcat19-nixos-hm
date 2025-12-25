d78c9979d9481d5324e51b13ebd7b24af65583f8
PopCat19 <atsuo11111@gmail.com>
2025-12-25 13:35:02 -0500

    fix(overlays): revert pinned overlays to use system instead of hostPlatform

--- configuration/flake/overlays/rocm-pinned.nix
+++ configuration/flake/overlays/rocm-pinned.nix
@@ -4,7 +4,7 @@ self: super: let
       url = "https://github.com/NixOS/nixpkgs/archive/18dd725c29603f582cf1900e0d25f9f1063dbf11.tar.gz";
       sha256 = "sha256-awS2zRgF4uTwrOKwwiJcByDzDOdo3Q1rPZbiHQg/N38=";
     }) {
-      hostPlatform = "x86_64-linux";
+      system = "x86_64-linux";
       config = {};
     };
 in {
