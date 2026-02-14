c8ad1aa344a28abc4a673985f216b8d6ddeab212
PopCat19 <atsuo11111@gmail.com>
2026-02-14 04:19:33 -0500

    refactor(home): add header and remove redundant comments in home-files [untested]

--- configuration/home/home_modules/home-files.nix
+++ configuration/home/home_modules/home-files.nix
@@ -1,19 +1,23 @@
+# home-files.nix
+#
+# Purpose: Manage home directory file symlinks and configuration
+#
+# This module:
+# - Links Hyprland configuration files
+# - Copies wallpaper directory for Noctalia
+# - Configures NPM global prefix
 { config, ... }:
 {
-  # **CONFIGURATION FILE MANAGEMENT**
-  # Manages symlinks for configuration files.
   home.file.".config/hypr" = {
     source = ../hypr_config;
     recursive = true;
   };
 
-  # Copy wallpaper directory for Noctalia wallpaper management
   home.file."wallpaper" = {
     source = ../wallpaper;
     recursive = true;
   };
 
-  # NPM global configuration for reproducible package management
   home.file.".npmrc".text = ''
     prefix=${config.home.homeDirectory}/.npm-global
   '';
