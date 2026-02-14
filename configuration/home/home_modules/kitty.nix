610fddf6db57138ed2b4b6c3c79738113a722c4d
PopCat19 <atsuo11111@gmail.com>
2026-02-14 04:22:43 -0500

    refactor(home): add header and sort settings alphabetically in kitty [untested]

--- configuration/home/home_modules/kitty.nix
+++ configuration/home/home_modules/kitty.nix
@@ -1,32 +1,40 @@
+# kitty.nix
+#
+# Purpose: Configure Kitty terminal emulator settings
+#
+# This module:
+# - Enables Kitty terminal
+# - Configures shell integration with Fish
+# - Sets cursor, window, and tab preferences
 _: {
   programs.kitty = {
     enable = true;
     settings = {
-      shell = "fish";
-      shell_integration = "enabled";
+      background_blur = 16;
       confirm_os_window_close = -1;
-      cursor_shape = "block";
       cursor_blink_interval = 0.5;
+      cursor_shape = "block";
       cursor_stop_blinking_after = 16.0;
       cursor_trail = 1;
-      scrollback_lines = 10000;
-      mouse_hide_wait = 3.0;
-      url_style = "curly";
       detect_urls = "yes";
-      repaint_delay = 10;
-      input_delay = 3;
-      sync_to_monitor = "yes";
+      dynamic_background_opacity = "yes";
       enable_audio_bell = "yes";
-      visual_bell_duration = 0.0;
+      input_delay = 3;
+      mouse_hide_wait = 3.0;
       remember_window_size = "yes";
-      window_border_width = 0.5;
-      window_margin_width = 8;
-      window_padding_width = 12;
+      repaint_delay = 10;
+      scrollback_lines = 10000;
+      shell = "fish";
+      shell_integration = "enabled";
+      sync_to_monitor = "yes";
       tab_bar_edge = "bottom";
       tab_bar_style = "separator";
       tab_separator = " | ";
-      dynamic_background_opacity = "yes";
-      background_blur = 16;
+      url_style = "curly";
+      visual_bell_duration = 0.0;
+      window_border_width = 0.5;
+      window_margin_width = 8;
+      window_padding_width = 12;
     };
   };
 }
