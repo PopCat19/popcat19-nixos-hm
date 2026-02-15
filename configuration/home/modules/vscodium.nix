e02b00d2a7683a4a3d6fa169ff82b0c5c98b11fe
PopCat19 <atsuo11111@gmail.com>
2026-02-14 21:18:48 -0500

    refactor(vscodium): use activation script for mutable settings
    Replace userSettings with activation script approach:
    - Write-once: creates settings.json if missing
    - Merge-on-change: uses jq to merge new defaults with existing
    - Preserves manual modifications to settings

--- configuration/home/modules/vscodium.nix
+++ configuration/home/modules/vscodium.nix
@@ -1,13 +1,60 @@
 # vscodium.nix
 #
-# Purpose: Configure VSCodium code editor with stylix theming and extensions
+# Purpose: Configure VSCodium with mutable settings and stylix theming
 #
 # This module:
-# - Enables VSCodium editor with essential extensions
-# - Integrates with Stylix for automatic theming
-# - Configures user settings and preferences
+# - Installs VSCodium with essential extensions
+# - Seeds default settings via activation script (write-once, merge-on-change)
+# - Preserves manual modifications to settings.json
 
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  settingsJson = builtins.toJSON {
+    "editor.minimap.enabled" = true;
+    "editor.minimap.renderCharacters" = true;
+
+    "extensions.experimental.affinity" = {
+      "ms-vscode.vscode-typescript-next" = 1;
+      "ms-vscode.vscode-json" = 1;
+      "vscode.yaml" = 1;
+    };
+
+    "files.autoSave" = "onFocusChange";
+    "files.autoSaveDelay" = 1000;
+    "files.insertFinalNewline" = true;
+    "files.trimFinalNewlines" = true;
+    "files.trimTrailingWhitespace" = true;
+    "files.watcherExclude" = {
+      "**/.git/objects/**" = true;
+      "**/.git/subtree-cache/**" = true;
+      "**/.hg/store/**" = true;
+      "**/node_modules/*/**" = true;
+      "**/target/debug/**" = true;
+    };
+
+    "git.autofetch" = true;
+    "git.confirmSync" = false;
+    "git.enableCommitSigning" = true;
+    "git.enableSmartCommit" = true;
+
+    "nix.enableLanguageServer" = true;
+    "nix.formatterPath" = "nixfmt";
+    "nix.serverPath" = "nixd";
+
+    "redhat.telemetry.enabled" = false;
+
+    "security.workspace.trust.enabled" = true;
+    "security.workspace.trust.startupPrompt" = "never";
+    "security.workspace.trust.untrustedFiles" = "open";
+
+    "terminal.integrated.defaultProfile.linux" = "fish";
+
+    "workbench.activityBar.location" = "bottom";
+    "workbench.colorTheme" = "Stylix";
+    "workbench.preferredDarkColorTheme" = "Stylix";
+    "workbench.preferredLightColorTheme" = "Stylix";
+  };
+in
 {
   programs.vscode = {
     enable = true;
@@ -19,51 +66,22 @@
       redhat.vscode-yaml
       rust-lang.rust-analyzer
     ];
-
-    profiles.default.userSettings = {
-      "editor.minimap.enabled" = true;
-      "editor.minimap.renderCharacters" = true;
-
-      "extensions.experimental.affinity" = {
-        "ms-vscode.vscode-typescript-next" = 1;
-        "ms-vscode.vscode-json" = 1;
-        "vscode.yaml" = 1;
-      };
-
-      "files.autoSave" = "onFocusChange";
-      "files.autoSaveDelay" = 1000;
-      "files.insertFinalNewline" = true;
-      "files.trimFinalNewlines" = true;
-      "files.trimTrailingWhitespace" = true;
-      "files.watcherExclude" = {
-        "**/.git/objects/**" = true;
-        "**/.git/subtree-cache/**" = true;
-        "**/.hg/store/**" = true;
-        "**/node_modules/*/**" = true;
-        "**/target/debug/**" = true;
-      };
-
-      "git.autofetch" = true;
-      "git.confirmSync" = false;
-      "git.enableCommitSigning" = true;
-      "git.enableSmartCommit" = true;
-
-      "nix.enableLanguageServer" = true;
-      "nix.formatterPath" = "nixfmt";
-      "nix.serverPath" = "nixd";
-
-      "redhat.telemetry.enabled" = false;
-
-      "security.workspace.trust.enabled" = true;
-      "security.workspace.trust.startupPrompt" = "never";
-      "security.workspace.trust.untrustedFiles" = "open";
-
-      "terminal.integrated.defaultProfile.linux" = "fish";
-
-      "workbench.activityBar.location" = "bottom";
-      "workbench.colorTheme" = "Stylix";
-      "workbench.preferredDarkColorTheme" = "Stylix";
-      "workbench.preferredLightColorTheme" = "Stylix";
-    };
   };
+
+  home.activation.vscodiumSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    settings_dir="$HOME/.config/VSCodium/User"
+    settings_file="$settings_dir/settings.json"
+
+    mkdir -p "$settings_dir"
+
+    if [ ! -f "$settings_file" ]; then
+      echo '${settingsJson}' > "$settings_file"
+    else
+      ${pkgs.jq}/bin/jq -s '.[0] * .[1]' \
+        "$settings_file" \
+        <(echo '${settingsJson}') \
+        > "$settings_file.tmp" \
+      && mv "$settings_file.tmp" "$settings_file"
+    fi
+  '';
 }
