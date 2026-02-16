89d2a0ed893ecab1187d2b6689d0dacfafd77474
PopCat19 <atsuo11111@gmail.com>
2026-02-16 02:58:53 -0500

    refactor(flake): decouple noctalia nixpkgs from main input

--- flake.nix
+++ flake.nix
@@ -40,10 +40,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    noctalia.url = "github:noctalia-dev/noctalia-shell";
 
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
