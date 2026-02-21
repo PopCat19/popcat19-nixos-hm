a82694ef5ca862dee3a34feb1a4dacd1e7b523f5
PopCat19 <atsuo11111@gmail.com>
2026-02-21 15:36:04 -0500

    feat(tablet): add udev rules for gaomon tablet device

--- configuration/system/modules/tablet.nix
+++ configuration/system/modules/tablet.nix
@@ -4,10 +4,16 @@
 #
 # This module:
 # - Enables OpenTabletDriver daemon and service
+# - Configures udev rules for tablet device access
 # - Configures tablet input support
 _: {
   hardware.opentabletdriver = {
     daemon.enable = true;
     enable = true;
   };
+
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", ATTRS{idVendor}=="b33f", ATTRS{idProduct}=="c4k3", TAG+="uaccess", TAG+="udev-acl"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="b33f", ATTRS{idProduct}=="c4k3", TAG+="uaccess", TAG+="udev-acl"
+  '';
 }
