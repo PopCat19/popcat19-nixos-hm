a8f10f6c78b9922e74c4f3121304913f3e41b40c
PopCat19 <atsuo11111@gmail.com>
2026-02-23 20:48:29 -0500

    feat(hosts): add moonlight and sunshine to all hosts

--- configuration/hosts/popcat19-surface0/configuration.nix
+++ configuration/hosts/popcat19-surface0/configuration.nix
@@ -14,6 +14,7 @@
     ./modules/clear-bdprochot.nix
     ./modules/hardware.nix
     ./modules/thermal-config.nix
+    ../../system/modules/sunshine.nix
   ];
 
   networking.hostName = userConfig.hostname;
--- configuration/hosts/popcat19-thinkpad0/configuration.nix
+++ configuration/hosts/popcat19-thinkpad0/configuration.nix
@@ -5,16 +5,21 @@
 # This module:
 # - Imports hardware configuration and profile preset
 # - Applies host-specific modules and settings
-{ lib, userConfig, ... }:
+{ pkgs, lib, userConfig, ... }:
 {
   imports = [
     ./hardware-configuration.nix
     ../../profiles/${userConfig.profile}.nix
     ./modules/hardware.nix
     ./modules/zram.nix
+    ../../system/modules/sunshine.nix
   ];
 
   networking.hostName = userConfig.hostname;
 
   services.displayManager.autoLogin.enable = lib.mkForce false;
+
+  environment.systemPackages = with pkgs; [
+    moonlight-qt
+  ];
 }
