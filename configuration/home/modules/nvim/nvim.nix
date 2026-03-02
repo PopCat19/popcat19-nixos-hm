225cd8347691d88133601cdd67f4d610ac097de3
PopCat19 <atsuo11111@gmail.com>
2026-03-02 18:23:00 -0500

    refactor(nvim): remove plugins, keymaps, lsp for now

--- configuration/home/modules/nvim/nvim.nix
+++ configuration/home/modules/nvim/nvim.nix
@@ -1,18 +1,15 @@
 # nvim.nix
 #
-# Purpose: Configure Neovim via nixvim with modular plugin setup
+# Purpose: Configure Neovim via nixvim
 #
 # This module:
 # - Imports nixvim home-manager module
-# - Delegates options, plugins, keymaps, and LSP to submodules
+# - Sets core options
 { inputs, ... }:
 {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
     ./options.nix
-    ./plugins.nix
-    ./keymaps.nix
-    ./lsp.nix
   ];
 
   programs.nixvim = {
