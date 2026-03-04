4414782757f61327ccca2f6a4d8386e478926259
PopCat19 <atsuo11111@gmail.com>
2026-03-04 02:14:39 -0500

    fix(helix): simplify to enable only

--- configuration/home/modules/helix.nix
+++ configuration/home/modules/helix.nix
@@ -4,20 +4,8 @@
 #
 # This module:
 # - Enables Helix via home-manager
-# - Configures basic editor settings
 _: {
   programs.helix = {
     enable = true;
-    settings = {
-      editor = {
-        line-number = "relative";
-        cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
-        };
-        selection-mode = "replace";
-      };
-    };
   };
 }
