04665e7444b201cca56b7efdbbb2b9bfe711296c
PopCat19 <atsuo11111@gmail.com>
2026-04-07 14:36:56 -0400

    fix(niri): disable tests to fix build panic in sandbox

--- configuration/system/modules/niri.nix
+++ configuration/system/modules/niri.nix
@@ -7,9 +7,15 @@
 # - Enables UWSM for display manager integration
 # - Configures XWayland support for X11 applications
 # - Intentionally minimal for learning stock behavior
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.niri.nixosModules.niri ];
+
+  programs.niri.package =
+    inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-stable.overrideAttrs
+      {
+        doCheck = false;
+      };
 
   programs.uwsm.enable = true;
 
