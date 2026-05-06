35a7ff5d217878df2cd56624ba38c865667ffabb
PopCat19 <atsuo11111@gmail.com>
2026-05-05 23:04:54 -0400

    fix(udev): tag OTD virtual tablets with ID_INPUT_TABLET for hyprland

--- configuration/system/modules/tablet.nix
+++ configuration/system/modules/tablet.nix
@@ -15,5 +15,13 @@ _: {
   services.udev.extraRules = ''
     KERNEL=="hidraw*", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="006d", TAG+="uaccess", TAG+="udev-acl"
     SUBSYSTEM=="usb", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="006d", TAG+="uaccess", TAG+="udev-acl"
+
+    # Tag OTD virtual devices as tablets for Wayland compositor tablet-v2 protocol
+    SUBSYSTEM=="input", ENV{DEVNAME}=="/dev/input/event*", \
+      ATTRS{name}=="OpenTabletDriver Virtual Tablet", \
+      ENV{ID_INPUT_TABLET}="1"
+    SUBSYSTEM=="input", ENV{DEVNAME}=="/dev/input/event*", \
+      ATTRS{name}=="OpenTabletDriver Virtual Artist Tablet", \
+      ENV{ID_INPUT_TABLET}="1"
   '';
 }
