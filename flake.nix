992698fc71f73eef56c1d62822ad954993255ce4
PopCat19 <atsuo11111@proton.me>
2026-05-15 06:58:24 -0400

    refactor(flake): move nixConfig to top level and remove output

--- flake.nix
+++ flake.nix
@@ -9,6 +9,21 @@
 {
   description = "NixOS multi-host configuration with profile presets";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://popcat19-shared.cachix.org"
+      "https://shimboot-systemd-nixos.cachix.org"
+      "https://numtide.cachix.org"
+      "https://nix-gaming.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "popcat19-shared.cachix.org-1:qqle0Ek1MtOHDkqu2srjAnbjwl41fRUP8pLd9ZDsMEQ="
+      "shimboot-systemd-nixos.cachix.org-1:vCWmEtJq7hA2UOLN0s3njnGs9/EuX06kD7qOJMo2kAA="
+      "numtide.cachix.org-1:2ps1kLBUWnLAnBIRTV6l6hEQuv59S++4Nux7496Z6tw="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
+  };
+
   inputs = {
     aagl = {
       url = "github:ezKEa/aagl-gtk-on-nix";
@@ -80,7 +95,6 @@
       imports = [
         ./flake-modules/nixos.nix
         ./flake-modules/formatter.nix
-        ./flake-modules/cachix.nix
       ];
     };
 }
