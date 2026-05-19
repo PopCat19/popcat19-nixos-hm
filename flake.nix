e3dd562e64819ec5737a8006e096070e458a5a2e
PopCat19 <atsuo11111@gmail.com>
2026-05-19 03:19:28 -0400

    fix(nix-on-droid): add nix-on-droid.cachix.org substituter
    Required for hardcoded proot-termux-static store paths in nix-on-droid modules

--- flake.nix
+++ flake.nix
@@ -24,12 +24,14 @@
       "https://numtide.cachix.org"
       "https://popcat19-shared.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://nix-on-droid.cachix.org"
     ];
     extra-trusted-public-keys = [
       "shimboot-systemd-nixos.cachix.org-1:vCWmEtJq7hA2UOLN0s3njnGs9/EuX06kD7qOJMo2kAA="
       "numtide.cachix.org-1:2ps1kLBUWnLAnBIRTV6l6hEQuv59S++4Nux7496Z6tw="
       "popcat19-shared.cachix.org-1:qqle0Ek1MtOHDkqu2srjAnbjwl41fRUP8pLd9ZDsMEQ="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-on-droid.cachix.org-1:jZuycLl0bOJAkMBNrCj4JnAjA31Os3R72xMRx1oCHFE="
     ];
   };
 
