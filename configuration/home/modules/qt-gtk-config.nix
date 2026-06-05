e4a9a689fe406e1aacf540ce13be262faf41df56
PopCat19 <atsuo11111@gmail.com>
2026-06-05 04:49:59 -0400

    chore(flake): update inputs, fix gtk4 theme conflict with mkDefault [untested]

--- configuration/home/modules/qt-gtk-config.nix
+++ configuration/home/modules/qt-gtk-config.nix
@@ -7,10 +7,10 @@
 # - Configures GTK bookmarks and Dolphin places
 # - Sets up Nemo file manager with custom actions
 # - Explicitly sets GTK4 theme to silence deprecation warning
-{ userConfig, config, ... }:
+{ userConfig, config, lib, ... }:
 {
   # Silence GTK4 theme deprecation warning (home.stateVersion < 26.05)
-  gtk.gtk4.theme = config.gtk.theme;
+  gtk.gtk4.theme = lib.mkDefault config.gtk.theme;
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
